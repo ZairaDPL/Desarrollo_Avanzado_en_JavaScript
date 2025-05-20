af8244b1b2dc345b3d4c090c222f3bba21f65a83
zaira_daniela_pacheco_limon <zairadpl18@gmail.com>
2025-05-19 23:05:05 -0600

    Función get para cada pokemon y renderizar aun no funciona

--- POKEDEX/main.js
+++ POKEDEX/main.js
@@ -23,11 +23,43 @@ async function getPokemons(offset) {
 
         //extraerDatos
         const data = await response.json();
-        console.log(data);
+        getSinglePokemon(data.results);
+        
         
     } catch (error) {
         container.innerHTML = `<p> ❌ Error al obtener los pokemones: ${error.message}</p>`
     }
+}
+
+async function getSinglePokemon(dataResults) {
+    for(const element of dataResults){
+        try {
+            const urlPokemon = await fetch(element.url);
+            if (!urlPokemon.ok) {
+                throw new Error(`Error al obtener detalles de ${element.name}`);
+            } 
+            const specificData = await urlPokemon.json();
+            renderPokemons(specificData);
+        } catch (error) {
+            container.innerHTML = `<p>❌ Error al obtener datos del pokemon ${element.name}: ${error.message} </p>`;
+        }
+    }
+}
+
+function renderPokemons(pokemon) {
+    //limpiar contenedor
+    container.innerHTML = "";
+    const card = document.createElement("div");
+    card.className = "card";
+    
+    //datos del pkemon
+    card.innerHTML = `
+        <h3>${pokemon.name}</h3>
+        <img class = "poke-image" src = "${pokemon.sprites.front_default}" alt="${details.name}">
+        <p>Tipo: ${pokemon.types.map(t => t.type.name).join(", ")}</p>
+    `;
+    
+    container.appendChild(card);
 }
 
 getPokemons(startPage);
